c90418d99119628137624b4aa88f3c3460de51b7
Patrik Lembke <blambi@chebab.com>
2011-12-29 22:47:36 +0100

    silly miss fixed

--- gruvdrift/wiki/models.py
+++ gruvdrift/wiki/models.py
@@ -6,10 +6,10 @@ class Revision( models.Model ):
     pub_date = models.DateTimeField( 'date published' )
     body = models.TextField()
     author = models.ForeignKey( User, unique=False )
+    page = models.ForeignKey( 'Page' )
 
 class Page( models.Model ):
     title = models.CharField( max_length=200, unique=True )
-    last_rev = models.ForeignKey( Revision )
     op_only = models.BooleanField( default=False ) # only op can write
 
     def __unicode__( self ):
