401978b1e29daa283b45bcc593283597b31bc4bb
Patrik Lembke <blambi@chebab.com>
2011-12-31 19:27:24 +0100

    missed one path...

--- gruvdrift/settings.py
+++ gruvdrift/settings.py
@@ -93,7 +93,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/patrik/programmering/python/gruvdrift/gruvdrift",
+    local_cfg.get( 'path', 'templates' )
 )
 
 INSTALLED_APPS = (
