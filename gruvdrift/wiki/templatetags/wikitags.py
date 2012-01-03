718a91d195bb67834c978e780dd330bcebbf2573
Lars Lindholm <larlin@lysator.liu.se>
2012-01-03 21:45:20 +0100

    Added css for images in wiki pages. Image HTML example in wikitags.

--- gruvdrift/wiki/templatetags/wikitags.py
+++ gruvdrift/wiki/templatetags/wikitags.py
@@ -10,5 +10,11 @@ def wikify( value ):
     """Mostly expansions to ReST so we get more recognised wiki links"""
     # wander around and fix all the links
     # TODO: add map link / image macro
+    # HTML for image:
+    # <div class="Wiki_image">
+    # <img src="/static/img/creeper.png" /><br />
+    # A creepy Creeper.
+    # </div>
+
     output = wiki_link.sub( r'[\1](/wiki/\1)\2', value )
     return output
