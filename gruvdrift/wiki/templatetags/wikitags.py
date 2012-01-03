c25d00a3138342e143e8fa14ef5ad8af70ce3dde
Patrik Lembke <blambi@chebab.com>
2012-01-03 23:54:02 +0100

    Wiki, image tag, might need a bit more css love sadly

--- gruvdrift/wiki/templatetags/wikitags.py
+++ gruvdrift/wiki/templatetags/wikitags.py
@@ -1,20 +1,30 @@
 from django import template
+from django.utils.html import escape, strip_tags
 import re
 
 register = template.Library()
 
 wiki_link = re.compile( "\[([A-Za-z_]+)\]([^(])" )
 
+wiki_image = re.compile( "\[(.*?)\]\((.*?)\)" )
+wiki_image_template = """<div class="Wiki_image">
+  <img src="%s" />
+  <p>%s</p>
+</div>"""
+
 @register.filter
 def wikify( value ):
     """Mostly expansions to ReST so we get more recognised wiki links"""
     # wander around and fix all the links
     # TODO: add map link / image macro
-    # HTML for image:
-    # <div class="Wiki_image">
-    # <img src="/static/img/creeper.png" /><br />
-    # A creepy Creeper.
-    # </div>
 
+    
     output = wiki_link.sub( r'[\1](/wiki/\1)\2', value )
+
+    for title, link in wiki_image.findall( output ):
+        if link.endswith( '.png' ) or link.endswith( '.jpeg' ) or link.endswith( '.jpg' ):
+            # we got an image
+            output = output.replace( "[%s](%s)" %( title, link ),
+                                     wiki_image_template %( strip_tags( link ),
+                                                            escape( title ) ) )
     return output
