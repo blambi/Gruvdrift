3be0f0b61911e0be60ca33bc2da881c1906978df
Patrik Lembke <blambi@chebab.com>
2012-01-14 18:58:04 +0100

    Added 'mark as offline' action for game sessions.

--- gruvdrift/wohaapi/admin.py
+++ gruvdrift/wohaapi/admin.py
@@ -1,8 +1,13 @@
 from wohaapi.models import Game_Sessions
 from django.contrib import admin
 
+def make_offline( modeladmin, request, queryset ):
+    queryset.update( online = False )
+make_offline.short_description = "Mark selected as offline"
+
 class Game_Admin( admin.ModelAdmin ):
     list_display = ('user', 'online', 'last_ping', 'timedout' )
+    actions = [ make_offline ]
 
 admin.site.register( Game_Sessions, Game_Admin )
 
