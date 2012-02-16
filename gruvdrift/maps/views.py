71bbe8a14ba7c0bf1d73d981cf69a92ce9d025e4
Lars Lindholm <larlin@lysator.liu.se>
2012-02-16 21:57:04 +0100

    Started working on new system for managing coordinates now can convert mc coordinates to all map pixel coordnates types (top, oblique, obliqueangle, iso). Can also convert back from iso pixel coordinates to mc coordinates.

--- gruvdrift/maps/views.py
+++ gruvdrift/maps/views.py
@@ -6,6 +6,9 @@ import settings
 import os
 
 def index( req, view = "iso" ):
+    modes = ["top", "oblique", "obliqueangle", "isometric"]
+    # This translates between images indexes and our map coords.
+    magic_num = { 'top': 100, 'iso': 300 }
     class MapImage:
         def __init__( self, x, y, sx, sy, view ):
             if os.path.exists( "%smaps/%s/0.%d.%d.png" %( settings.MEDIA_ROOT, view, x, y ) ):
@@ -16,10 +19,69 @@ def index( req, view = "iso" ):
             self.center = False
             self.x = x - sx
             self.y = y - sy
-    
-    # This translates between images indexes and our map coords.
-    magic_num = { 'top': 100, 'iso': 300 }
-    
+
+
+    class Projection:
+        def __init__( self, json_name):
+        
+            if os.path.exists(json_name):
+                self.meta = json.loads(file(json_name, 'r').read()[16:-1])["world"]
+            else:
+                error = "true!"
+                #TODO: Report error for real here!
+            
+            self.projections = [self.to_top, self.to_oblique, \
+                                self.to_obliqueangle, self.to_iso]
+                                
+            self.projection = self.projections[self.meta["mode"]]
+        
+        def project( self, x, y, z):
+            crop_comp = self.projection(0, 0, 0)
+            slize = self.projection(x, y, z)
+            
+            slize[0] -= crop_comp[0] - self.meta["cx"]
+            slize[1] -= crop_comp[1] - self.meta["cy"]
+            
+            return slize
+        
+        def invert( self, x, y):
+            crop_comp = self.projection(0, 0, 0)
+            x += crop_comp[0] - self.meta["cx"]
+            y += crop_comp[1] - self.meta["cy"]
+            
+            mc = self.from_iso(x, y)
+            
+            mc[0] += self.meta["mn_x"]
+            mc[2] += self.meta["mn_z"]
+            
+            return mc
+        
+        def to_top( self, x, y, z):
+            x -= self.meta["mn_x"]
+            z -= self.meta["mn_z"]
+            return [ self.meta["dz"] - z - 1, x]
+        
+        def to_oblique( self, x, y, z):
+            x -= self.meta["mn_x"]
+            z -= self.meta["mn_z"]
+            return [ self.meta["dz"] - z - 1 + x, x]
+        
+        def to_obliqueangle( self, x, y, z):
+            x -= self.meta["mn_x"]
+            z -= self.meta["mn_z"]
+            return [ self.meta["dz"] - z - 1 + x, self.meta["dy"] - y - 1 + z + x]
+        
+        def to_iso( self, x, y, z):
+            x -= self.meta["mn_x"]
+            z -= self.meta["mn_z"]
+            return [ 2 * (self.meta["dz"] - z - 1 + x), \
+                     2 * (self.meta["dy"] - y - 1) + z + x]
+        
+        def from_iso( self, x, y):
+            x = x / 2 - self.meta["dz"]
+            z = 0
+            y = y - 2 * self.meta["dy"] + 2 * z
+            return [(3 + x + y)/2, z, (1 - x +y)/2]
     
     # Fix which view to use if none or a bad one use iso.
     if not view in ["iso", "top"]:
