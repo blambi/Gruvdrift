1087bbf218bb2c778137feac5dd01b83a51b0ea2
Lars Lindholm <larlin@lysator.liu.se>
2012-02-17 15:48:40 +0100

    Added testing methods not sure everything works.

--- gruvdrift/maps/views.py
+++ gruvdrift/maps/views.py
@@ -19,69 +19,6 @@ def index( req, view = "iso" ):
             self.center = False
             self.x = x - sx
             self.y = y - sy
-
-
-    class Projection:
-        def __init__( self, json_name):
-        
-            if os.path.exists(json_name):
-                self.meta = json.loads(file(json_name, 'r').read()[16:-1])["world"]
-            else:
-                error = "true!"
-                #TODO: Report error for real here!
-            
-            self.projections = [self.to_top, self.to_oblique, \
-                                self.to_obliqueangle, self.to_iso]
-                                
-            self.projection = self.projections[self.meta["mode"]]
-        
-        def project( self, x, y, z):
-            crop_comp = self.projection(0, 0, 0)
-            slize = self.projection(x, y, z)
-            
-            slize[0] -= crop_comp[0] - self.meta["cx"]
-            slize[1] -= crop_comp[1] - self.meta["cy"]
-            
-            return slize
-        
-        def invert( self, x, y):
-            crop_comp = self.projection(0, 0, 0)
-            x += crop_comp[0] - self.meta["cx"]
-            y += crop_comp[1] - self.meta["cy"]
-            
-            mc = self.from_iso(x, y)
-            
-            mc[0] += self.meta["mn_x"]
-            mc[2] += self.meta["mn_z"]
-            
-            return mc
-        
-        def to_top( self, x, y, z):
-            x -= self.meta["mn_x"]
-            z -= self.meta["mn_z"]
-            return [ self.meta["dz"] - z - 1, x]
-        
-        def to_oblique( self, x, y, z):
-            x -= self.meta["mn_x"]
-            z -= self.meta["mn_z"]
-            return [ self.meta["dz"] - z - 1 + x, x]
-        
-        def to_obliqueangle( self, x, y, z):
-            x -= self.meta["mn_x"]
-            z -= self.meta["mn_z"]
-            return [ self.meta["dz"] - z - 1 + x, self.meta["dy"] - y - 1 + z + x]
-        
-        def to_iso( self, x, y, z):
-            x -= self.meta["mn_x"]
-            z -= self.meta["mn_z"]
-            return [ 2 * (self.meta["dz"] - z - 1 + x), \
-                     2 * (self.meta["dy"] - y - 1) + z + x]
-        
-        def from_iso( self, x, y):
-            x = x / 2 - self.meta["dz"]
-            z = 0
-            y = y - 2 * self.meta["dy"] + 2 * z
-            return [(3 + x + y)/2, z, (1 - x +y)/2]
     
     # Fix which view to use if none or a bad one use iso.
     if not view in ["iso", "top"]:
