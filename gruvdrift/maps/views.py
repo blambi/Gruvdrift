1254910f99c759039c4fdfc2c1db6532af42bca3
Patrik Lembke <blambi@chebab.com>
2012-06-24 16:13:04 +0200

    Map: Fixed the issue with people not entering all required info

--- gruvdrift/maps/views.py
+++ gruvdrift/maps/views.py
@@ -1,9 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.utils import simplejson as json
-import settings
-import os
 
 def index( req ):
     c = dict()
@@ -13,13 +10,21 @@ def index( req ):
         opts.append("worldname=%s" % req.GET['world'])
     if req.GET.has_key('map'):
         opts.append("mapname=%s" % req.GET['map'])
+
     if req.GET.has_key('x'):
         opts.append("x=%d" % int(req.GET['x']))
+    else:
+        opts.append("x=0")
+
     if req.GET.has_key('z'):
         opts.append("z=%d" % int(req.GET['z']))
+    else:
+        opts.append("z=0")
+
     if req.GET.has_key('zoom'):
         opts.append("zoom=%d" % int(req.GET['zoom']))
-    
+
+
     c['options'] = "&".join(opts)
     
     return render_to_response( "maps/index.html", RequestContext( req, c ) )
